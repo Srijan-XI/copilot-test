bf8a84a198088048cdc5bbb662a1a428ceef1351
Srijan Kumar <142709398+Srijan-XI@users.noreply.github.com>
2025-10-02 13:24:55 +0000

    Fix formatting in activities section by removing extra whitespace

--- src/static/app.js
+++ src/static/app.js
@@ -21,6 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const spotsLeft = details.max_participants - details.participants.length;
         
         // Create participants list HTML
+        
         let participantsHtml = '';
         if (details.participants.length > 0) {
           const participantsList = details.participants
